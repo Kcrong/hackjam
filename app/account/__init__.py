8b5224d453c36661d3486a52ee22a0f0ceea7861
kcrong <kcrong@kim82536.pe.kr>
2016-03-05 23:02:44 +0900

    remove time stamp source

--- app/account/__init__.py
+++ app/account/__init__.py
@@ -6,34 +6,4 @@ from werkzeug.exceptions import BadRequestKeyError
 
 account_blueprint = Blueprint('account', __name__, template_folder='../templates/account/')
 
-"""
-@account_blueprint.before_request
-def account_before_request():
-    if request.method == 'POST':
-        pass
-    elif request.method == 'GET':
-        try:
-            session_time = session['time']
-        except KeyError:
-            session_time = session['time'] = str(time())
-        try:
-            request_time = request.args['time']
-        except BadRequestKeyError:
-            session['time'] = str(time())
-            if len(request.args) != 0:
-                return redirect(request.url + '&time=' + session['time'])
-            else:
-                return redirect(request.url + '?time=' + session['time'])
-
-        if session_time != request_time:
-            session['time'] = str(time())
-
-            if len(request.args) != 0:
-                redirect_url = request.url + '&time=' + session['time']
-            else:
-                redirect_url = request.url + '?time=' + session['time']
-            session['time'] = 0
-            return redirect(redirect_url)
-"""
-
 from . import views
