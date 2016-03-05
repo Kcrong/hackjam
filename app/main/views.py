fa7e2f885739eb35bcffd9dcc36f9e3d7546d01f
kcrong <kcrong@kim82536.pe.kr>
2016-03-05 22:48:04 +0900

    move static file to static files server

--- app/main/views.py
+++ app/main/views.py
@@ -16,31 +16,6 @@ def index():
     return render_template('index.html')
 
 
-@main_blueprint.route('/css/<path:filename>')
-def css_static(filename):
-    return send_from_directory(app.root_path + '/static/css/', filename)
-
-
-@main_blueprint.route('/js/<path:filename>')
-def js_static(filename):
-    return send_from_directory(app.root_path + '/static/js/', filename)
-
-
-@main_blueprint.route('/img/<path:filename>')
-def img_static(filename):
-    return send_from_directory(app.root_path + '/static/img/', filename)
-
-
-@main_blueprint.route('/fonts/<path:filename>')
-def font_static(filename):
-    return send_from_directory(app.root_path + '/static/fonts/', filename)
-
-
-@main_blueprint.route('/favicon.ico')
-def favicon():
-    return send_from_directory(app.root_path + '/static/', 'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
-
 @main_blueprint.route('/prob_image/<path:filename>')
 def prob_image(filename):
     return send_from_directory(main_blueprint.root_path + '/../prob/prob_images/', filename)
