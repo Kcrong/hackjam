4fa221ad9d9d7db016765c8cbf7a3e147c336b61
kcrong <kcrong@kim82536.pe.kr>
2016-04-12 16:51:51 +0900

    Remove db_init command at manage

--- manage.py
+++ manage.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 from app import create_app, manager
-from app import init_db
 
 app = create_app()
 
@@ -10,13 +9,6 @@ app = create_app()
 @manager.command
 def run():
     app.run(host='0.0.0.0', debug=True)
-
-
-@manager.command
-def db_init():
-    init_db()
-    print("Success to Generate db init data.")
-
 
 if __name__ == "__main__":
     manager.run()
