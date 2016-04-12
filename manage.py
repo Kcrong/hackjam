a535459d8e1309cadf8f8a9c5b7e123cc75ffc53
kcrong <kcrong@kim82536.pe.kr>
2016-04-12 21:12:44 +0900

    manager command 에 init_db 추가

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from app import create_app, manager
+from app.models import *
 
 app = create_app()
 
@@ -9,6 +10,27 @@ app = create_app()
 @manager.command
 def run():
     app.run(host='0.0.0.0', debug=True)
+
+
+@manager.command
+def init_db():
+    u = User()
+    u.nickname = 'admin'
+    u.userid = 'Administrator'
+    u.userpw = 'Administrator'
+    u.active = False
+
+    p = Prob()
+    p.title = 'signup'
+    p.key = 'Hello World'
+    p.active = False
+    p.maker = u
+
+    u.success_prob = [p]
+
+    db.session.add_all([u, p])
+    db.session.commit()
+
 
 if __name__ == "__main__":
     manager.run()
